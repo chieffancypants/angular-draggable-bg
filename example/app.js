1c085e690809f1203f55600261a8900ae514f55a
Wes Cruver <chieffancypants@gmail.com>
2015-09-14 18:32:38 +0200

    initial work on converting to percentages

--- example/app.js
+++ example/app.js
@@ -3,11 +3,11 @@
 angular.module('draggableBgExample', ['chieffancypants.draggableBg'])
   .controller('ExampleCtrl', function ($scope, $rootScope, $http, $timeout) {
     $scope.images = [
-      {url: 'http://c2.staticflickr.com/6/5695/20824517862_866a0bb7c1_h.jpg', posX: -100, posY: 0},
-      {url: 'http://c2.staticflickr.com/6/5767/20810643016_fd4af72a49_b.jpg', posX: -40, posY: 0},
+      {url: 'http://c2.staticflickr.com/6/5695/20824517862_866a0bb7c1_h.jpg', posX: 20, posY: 0},
+      {url: 'http://c2.staticflickr.com/6/5767/20810643016_fd4af72a49_b.jpg', posX: 0, posY: 75},
     ];
 
-    $scope.anotherImage = { url: 'http://c1.staticflickr.com/1/620/20660927439_c282c95dc2_h.jpg'  };
+    $scope.anotherImage = { url: 'http://c1.staticflickr.com/1/620/20660927439_c282c95dc2_h.jpg' };
 
     $rootScope.$on('draggableBg:repositioned', function (event, args) {
       console.log($scope.images, args);
